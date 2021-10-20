953c033f3918b02a699faea2e5c60ad157cc87bf
Tristan Tien-Mi-Tiee <tristantienmitiee@gmail.com>
2021-10-20 11:38:03 +0400

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,23 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBtnClose = document.querySelectorAll(".modal-btn__close");
 
 // launch modal event
+// Boucle, pour chaque bouton au clique faire apparaître le modal
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Boucle, pour chaque bouton au clique faire apparaître le modal
+modalBtnClose.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
+  // Prend le modal pour le faire apparaître
   modalbg.style.display = "block";
+}
+
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
