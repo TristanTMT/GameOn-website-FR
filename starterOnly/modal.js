f1857b3560b028d11dca0441faae4115ae967f14
Tristan Tien-Mi-Tiee <tristantienmitiee@gmail.com>
2021-10-21 11:36:49 +0400

    id form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,6 +15,14 @@ const modalBtnClose = document.querySelector(".modal-btn__close");
 //===========================================================================================
 
 // DOM Elements <input> for form
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const email = document.getElementById('email');
+const birthdate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
+//===========================================================================================
+
+// DOM Elements <input> for form ERROR
 const firstNameError = document.getElementById('firstNameError');
 const lastNameError = document.getElementById('lastNameError');
 const emailError = document.getElementById('emailError');
@@ -22,18 +30,16 @@ const birthdateError = document.getElementById('birthdateError');
 const quantityError = document.getElementById('quantityError');
 //===========================================================================================
 
-/* FONCTIONS */
-// launch modal form
-function launchModal() {
-  // Prend le modal pour le faire apparaître
-  modalbg.style.display = "block";
-}
-
-function closeModal() {
-  // Prend la classe du modal pour le faire disparître
-  modalbg.style.display = "none";
-}
+// LES MESSAGES D'ERREURS A IMPLEMENTER
+const arrMessageError = [
+  "Veuillez entrer 2 caractères ou plus pour le champ du prénon.", 
+  "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
+  "J'attends un e-mail, mon cher !",
+  "Vous devez entrer votre date de naissance."];
+  "Vous devez choisir une ville.", 
+  "Vous devez vérifier que vous acceptez les termes et conditions.",
 //===========================================================================================
+
 
 // launch modal event
 // Boucle, pour chaque bouton au clique faire apparaître le modal
@@ -42,6 +48,29 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // Evenement pour fermer le modal
 // modalBtnClose.addEventListener("click", e => modalbg.style.display = "none");
 modalBtnClose.addEventListener("click", closeModal);
+
+/* FONCTIONS */
+// launch modal form
+function launchModal() {
+  // Prend le modal pour le faire apparaître
+  modalbg.style.display = "block";
+};
+
+function closeModal() {
+  // Prend la classe du modal pour le faire disparître
+  modalbg.style.display = "none";
+}
+
+function checkFirstName() {
+  if(!firstName.value){
+    console.log("Hello")
+  }
+};
+
+//===========================================================================================
+
+
+
 
 
 email.addEventListener("keyup", event => {
