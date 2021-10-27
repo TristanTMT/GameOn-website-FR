aab5f5c5a0f8ba011ca74fb35ac5f9034a84b41b
Tristan Tien-Mi-Tiee <tristantienmitiee@gmail.com>
2021-10-27 10:26:41 +0400

    responsive

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,4 +1,5 @@
-const $signInForm = document.querySelector(".sign-in-form");
+// Target le formulaire par son ID
+const $signInForm = document.querySelector("#sign-in-form");
 
 //FAIRE LE RESPONSIVE ============================== IMPORTANT
 function editNav() {
@@ -29,6 +30,16 @@ const email = document.getElementById('email');
 const birthdate = document.getElementById('birthdate');
 const quantity = document.getElementById('quantity');
 const cgu = document.getElementById('checkbox1');
+// TABLEAU POUR TOUTES LES VILLES
+const checkboxCity = [
+  document.getElementById("location1"),
+  document.getElementById("location2"),
+  document.getElementById("location3"),
+  document.getElementById("location4"),
+  document.getElementById("location5"),
+  document.getElementById("location6"),
+];
+//===========================================================================================
 //===========================================================================================
 
 // DOM Elements <input> for form ERROR
@@ -39,6 +50,8 @@ const birthdateError = document.getElementById('birthdateError');
 const quantityError = document.getElementById('quantityError');
 const checkedCityError = document.getElementById("checkedCityError");
 const cguError = document.getElementById("CGUError");
+
+const inputStyleError = document.getElementsByTagName("input");
 //===========================================================================================
 
 // LES MESSAGES D'ERREURS A IMPLEMENTER AJOUTER COULER ROUGE
@@ -49,17 +62,6 @@ const arrMessageError = [
   "Vous devez entrer votre date de naissance.",
   "Vous devez choisir une ville.",
   "Vous devez vérifier que vous acceptez les termes et conditions.",
-];
-//===========================================================================================
-
-// TABLEAU POUR TOUTES LES VILLES
-const checkboxCity = [
-  document.getElementById("location1"),
-  document.getElementById("location2"),
-  document.getElementById("location3"),
-  document.getElementById("location4"),
-  document.getElementById("location5"),
-  document.getElementById("location6"),
 ];
 //===========================================================================================
 
@@ -84,59 +86,111 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// Vérification du prénom
+
 const checkFirstName = () => {
   if(!firstName.value || firstName.value.length < 2){
     firstNameError.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+    firstName.style.background = "#FFBABA"
+    return false;
+    // firstNameError.parentElement.setAttribute("data-error-visible", "true")
   }
   else if (firstName.value) {
     firstNameError.style.display = "none";
+    firstName.style.background = ""
     return true;
   }
 };
+
+// Vérification du nom de famille
 
 const checkedLastName = () => {
   if(!lastName.value || lastName.value.length < 2){
     lastNameError.textContent = "Veuillez entrer 2 caractères ou plus pour le nom de famille."
+    lastName.style.background = "#FFBABA"
+    return false;
   }
   else {
     lastNameError.style.display = "none";
+    lastName.style.background = ""
     return true;
   }
 };
 
+// Vérification de l'email
+
 const checkedEmail = () => {
-  if(!email.value){
-    emailError.textContent = "Veuillez entrer 2 caractères ou plus pour le nom de famille."
+  const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(email.value.match(regexEmail)){
+    emailError.style.display = "none"
+    email.style.background = ""
+    return true;
+  }
+  else if(!email.value){
+    console.log('email vide');
+    emailError.textContent = "Veuillez entrer une adresse mail valide."
+    email.style.background = "#FFBABA"
+    return false;
   }
   else {
     emailError.style.display = "none";
+    email.style.background = ""
     return true;
   }
+};
+
+// Vérification de la date d'anniversaire
+
+const getAgeInYears = () => {
+  const today = new Date();
+  const birthdayDate = new Date(birthdate.value);
+  const dateInMl = today.getTime() - birthdayDate.getTime(); // Soustraction pour obtenir l'âge
+  const age = dateInMl / 31536000000 // Retourne les millisecondes en années
+
+  return age;
 };
 
 const checkedBirthdate = () => {
   if(!birthdate.value){
     birthdateError.textContent = "Vous devez entrer votre date de naissance."
+    birthdate.style.background = "#FFBABA"
+    return false;
+  }
+  else if( getAgeInYears() < 12 ) {
+    console.log('Il faut avoir plus de 12 ans');
+    birthdateError.textContent = "Il faut avoir plus de 12 ans"
+    birthdate.style.background = "#FFBABA"
+    return false;
   }
   else {
     birthdateError.style.display = "none";
+    birthdate.style.background = ""
     return true;
   }
 };
+//===========================================================================================
+
+// Vérification de la quantité
 
 const checkedQuantity = () => {
   if(!quantity.value){
     quantityError.textContent = "Veuillez entrer un nombre en 0 et 99."
+    quantity.style.background = ""
+    return false;
   }
   else {
     quantityError.style.display = "none";
+    quantity.style.background = ""
     return true;
   }
 };
 
+// Vérification de le ville
+
 const checkedCity = () => {
   if(!checkboxCity[0].checked && !checkboxCity[1].checked && !checkboxCity[2].checked && !checkboxCity[3].checked && !checkboxCity[4].checked && !checkboxCity[5].checked){
     checkedCityError.textContent = "Veuillez sélectionner une ville."
+    return false;
   }
   else {
     checkedCityError.style.display = "none";
@@ -144,12 +198,17 @@ const checkedCity = () => {
   }
 };
 
+// Vérification des conditions générales
+
 const checkedCGU = () => {
   if(!cgu.value){
     cguError.textContent = "Vous devez vérifier que vous acceptez les termes et conditions."
+    cgu.style.background = "#FFBABA"
+    return false;
   }
   else {
     cguError.style.display = "none";
+    cgu.style.background = ""
     return true;
   }
 };
@@ -169,12 +228,17 @@ btnModalEnd.addEventListener("click", closeEndModal);
 
 
 $signInForm.addEventListener("submit", event => {
+  // Permet de ne pas renvoyer sur une nouvelle page vide
   event.preventDefault();
 
-  if( checkFirstName() && checkedLastName() && checkedEmail() && checkedQuantity() && checkedCity() && checkedCGU() ){
+  if( checkFirstName() && checkedLastName() && checkedEmail() && checkedBirthdate() && checkedQuantity() && checkedCity() && checkedCGU() ){
     
     closeModal();
     launchModalEnd();
+    /* La méthode HTMLFormElement.reset() restaure les valeurs par défaut des éléments du formulaire.
+    Cette méthode fait la même chose que lorsqu'on clique sur un bouton de type reset. */
+    document.getElementById("sign-in-form").reset();
+    return true;
   }
   
 })
